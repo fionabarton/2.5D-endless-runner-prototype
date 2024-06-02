52804931915157311fe8da7911badddda9c93237
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-06-01 22:14:30 -0500

    Add RespawnTrigger.cs
    Add RespawnTrigger.cs, which on trigger enter, move player GameObject back to starting position.

---
+++ WorldObjects/RespawnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On trigger enter, move player GameObject back to starting position.
+public class RespawnTrigger : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D coll) {
+        // If tag of (coll) GameObject is (Player)...
+        if (coll.gameObject.tag == "Player") {
+            // ...reset player position
+			PlayerManager.S.MoveToStartingPosition();
+        }
+	}
+}

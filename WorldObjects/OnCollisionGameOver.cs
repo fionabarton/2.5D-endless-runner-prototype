80bd03477bf57d444b4ee2995b05059221c1938e
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-07-13 17:33:19 -0500

    Add OnCollisionGameOver.cs
    Add OnCollisionGameOver.cs, which on collision with objects of a specified Unity tag, ends the game.

---
+++ WorldObjects/OnCollisionGameOver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On collision with objects of a specified Unity tag, ends the game.
+public class OnCollisionGameOver : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public string tagName = "Player";
+
+    private void OnTriggerEnter(Collider other) {
+        // If tag of (other) GameObject matches (tagName)...
+        if (other.gameObject.tag == tagName) {
+            if (!PlayerManager.S.isShielded) {
+                // ...end the game
+                PlayerManager.S.GameOver();
+            } else {
+                // ...deactivate the player's shield
+                PlayerManager.S.DeactivateShield();
+            } 
+        }
+    }
+}

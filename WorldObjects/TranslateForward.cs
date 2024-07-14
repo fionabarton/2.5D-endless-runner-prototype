147205f66538f83dfeac5cad05b41256efd46dab
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-07-14 14:16:39 -0500

    Replace local move speed with ObjectSpawner field

--- WorldObjects/TranslateForward.cs
+++ WorldObjects/TranslateForward.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 // Moves an object forward on the z-axis at an adjustable rate.
 public class TranslateForward : MonoBehaviour {
-    [Header("Set in Inspector")]
-    public float    moveSpeed = 1;
-
     void FixedUpdate() {
         // Translate object forward on z-axis
-        transform.Translate(Vector3.forward * Time.fixedDeltaTime * moveSpeed);
+        transform.Translate(Vector3.forward * Time.fixedDeltaTime * ObjectSpawner.S.moveSpeed);
     }
 }

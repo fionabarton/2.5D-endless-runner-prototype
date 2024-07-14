1cbd6bab7eed7d2cdd970cb49cbc31f61c1fbf09
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-07-14 14:32:30 -0500

    Rename file so it starts with ‘OnCollision’

---
+++ WorldObjects/OnCollisionDestroy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On collision with objects of a specified Unity tag, destroy this GameObject.
+public class OnCollisionDestroy : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public string tagName = "Player";
+
+    private void OnTriggerEnter(Collider other) {
+        // If tag of (other) GameObject matches (tagName)...
+        if (other.gameObject.tag == tagName) {
+            // ...destroy this GameObject
+            Destroy(gameObject);
+        }
+    }
+}

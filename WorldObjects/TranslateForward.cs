51ddb357a21773b5f994e9de4853479e12bc54a3
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-08-10 15:57:33 -0500

    Add isMoving boolean field to disable movement

--- WorldObjects/TranslateForward.cs
+++ WorldObjects/TranslateForward.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 // Moves an object forward on the z-axis at an adjustable rate.
 public class TranslateForward : MonoBehaviour {
+    [Header("Set Dynamically")]
+    public bool isMoving = true;
+
     void FixedUpdate() {
         // Translate object forward on z-axis
-        transform.Translate(Vector3.forward * Time.fixedDeltaTime * ObjectSpawner.S.moveSpeed);
+        if (isMoving) {
+            transform.Translate(Vector3.forward * Time.fixedDeltaTime * ObjectSpawner.S.moveSpeed);
+        }  
     }
 }

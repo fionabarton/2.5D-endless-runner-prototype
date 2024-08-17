ce6e0d5c65a6ea94998349069a34fba06b88ce4c
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-08-17 16:35:06 -0500

    Add master audio volume slider

--- UI/OptionsMenu.cs
+++ UI/OptionsMenu.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 // A menu of user-adjustable general options such as audio volume. 
 public class OptionsMenu : MonoBehaviour {
     [Header("Set in Inspector")]
+    public Slider   masterVolSlider;
+
+    //public Button muteAudioButton;
+    //public TMPro.TextMeshProUGUI muteAudioButtonText;
+    
     public Button   goBackButton;
 
     [Header("Set Dynamically")]
@@ -19,14 +24,26 @@ public class OptionsMenu : MonoBehaviour {
     }
 
     void Start() {
-        // Add listener
+        // Add listeners to sliders
+        masterVolSlider.onValueChanged.AddListener(delegate { AudioManager.S.SetMasterVolume((masterVolSlider.value)); });
         goBackButton.onClick.AddListener(delegate { Deactivate(true); });
 
         Invoke("OnStart", 0.1f);
     }
 
     void OnStart() {
+        Invoke("GetPlayerPrefs", 0.1f);
+
         Deactivate();
+    }
+
+    void GetPlayerPrefs() {
+        if (PlayerPrefs.HasKey("Master Volume")) {
+            masterVolSlider.value = PlayerPrefs.GetFloat("Master Volume");
+            AudioManager.S.SetMasterVolume(masterVolSlider.value);
+        } else {
+            AudioManager.S.SetMasterVolume(0.25f);
+        }
     }
 
     public void ActivateMenu() {

0fe8766b5d1f8626a5742b9627fd27ac10ab41b7
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-08-17 17:40:55 -0500

    Add on level select, update main menu UI

--- UI Menus/MainMenu.cs
+++ UI Menus/MainMenu.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 // Provides navigation to all user-accessible parts of the game.
 public class MainMenu : MonoBehaviour {
     [Header("Set in Inspector")]
-    public Button   startGameButton;
-    public Button   levelButton;
-    public Button   highScoreMenuButton;
-    public Button   optionsMenuButton;
+    public Button                   startGameButton;
+    public Button                   levelButton;
+    public Button                   highScoreMenuButton;
+    public Button                   optionsMenuButton;
+
+    public TMPro.TextMeshProUGUI    levelValueText;
+
 
     [Header("Set Dynamically")]
     // Single instance of this class, which provides global acess from other scripts
@@ -61,5 +64,10 @@ public class MainMenu : MonoBehaviour {
 
         // Activate main menu
         gameObject.SetActive(true);
+    }
+
+    // After level selection, update level, move, & spawn speed text values
+    public void UpdateUI() {
+        levelValueText.text = ScoreManager.S.level + "\n" + ObjectSpawner.S.moveSpeed + "\n" + ObjectSpawner.S.spawnSpeed;
     }
 }

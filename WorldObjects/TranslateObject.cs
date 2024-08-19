a1450287315698711a83db97b19e7d130a857dee
fionabarton <56236345+fionabarton@users.noreply.github.com>
2024-08-19 14:08:29 -0500

    Add TranslateObject.cs
    Add TranslateObject.cs, which moves an object in a single, specified direction at a specified speed.

---
+++ WorldObjects/TranslateObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Moves an object in a single, specified direction at a specified speed.
+public class TranslateObject : MonoBehaviour {
+    [Header("Set Dynamically")]
+    public bool     isMoving = true;
+
+    public Vector3  direction = Vector3.forward;
+
+    void FixedUpdate() {
+        // Translate object towards desired direction
+        if (isMoving) {
+            transform.Translate(direction * Time.fixedDeltaTime * ObjectSpawner.S.moveSpeed);
+        }
+    }
+
+    public void SetDirection(Vector3 _direction) {
+        direction = _direction;
+    }
+}
